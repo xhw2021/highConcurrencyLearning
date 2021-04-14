356e7fab3f821ae1bdffedb631dc184dd9e0b2d1
xhw2021 <82018182+xhw2021@users.noreply.github.com>
2021-04-14 19:11:23 +0800

    Add files via upload

---
+++ codes/Chapter3/CountDownLatchDemo.java
@@ -0,0 +1,43 @@
+package Chapter03;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @ClassName CountDownLatchDemo
+ * @Description TODO
+ * @Author 22936
+ * @Date 2021/4/13 17:02
+ * @Version 1.0
+ */
+
+public class CountDownLatchDemo implements Runnable {
+    static final CountDownLatch end = new CountDownLatch(10);
+    static final CountDownLatchDemo demo = new CountDownLatchDemo();
+
+    @Override
+    public void run() {
+        try{
+            Thread.sleep(new Random().nextInt(10)*1000);
+            System.out.println("Check complete!");
+            end.countDown();
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        for(int i = 0;i<10;i++){
+            exec.submit(demo);
+        }
+
+        end.await();
+
+        System.out.println("Fire!");
+
+        exec.shutdown();
+    }
+}
